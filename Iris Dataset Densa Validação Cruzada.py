eb65eee5b1c5d089b8330bae60c1b6ff63f82550
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2020-08-07 15:54:30 -0300

    Update Iris Dataset Densa Validação Cruzada.py

--- Iris Dataset Densa Validação Cruzada.py
+++ Iris Dataset Densa Validação Cruzada.py
@@ -1,3 +1,7 @@
+# Livro Ciência de Dados e Aprendizado de Máquina - https://www.amazon.com.br/dp/B07X1TVLKW
+# Livro Inteligência Artificial com Python - Redes Neurais Intuitivas - https://www.amazon.com.br/dp/B087YSVVXW
+# Livro Redes Neurais Artificiais - https://www.amazon.com.br/dp/B0881ZYYCJ
+
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
@@ -43,3 +47,12 @@ resultadoValCruzada = cross_val_score(estimator = classificadorValCruzada,
 
 media = resultadosValCruzada.mean()
 desvio = resultadosValCruzada.std()
+
+# Livro Python do ZERO à Programação Orientada a Objetos - https://www.amazon.com.br/dp/B07P2VJVW5
+# Livro Programação Orientada a Objetos com Python - https://www.amazon.com.br/dp/B083ZYRY9C
+# Livro Tópicos Avançados em Python - https://www.amazon.com.br/dp/B08FBKBC9H
+# Livro Ciência de Dados e Aprendizado de Máquina - https://www.amazon.com.br/dp/B07X1TVLKW
+# Livro Inteligência Artificial com Python - Redes Neurais Intuitivas - https://www.amazon.com.br/dp/B087YSVVXW
+# Livro Redes Neurais Artificiais - https://www.amazon.com.br/dp/B0881ZYYCJ
+# Livro Análise Financeira com Python - https://www.amazon.com.br/dp/B08B6ZX6BB
+# Livro Arrays com Python + Numpy - https://www.amazon.com.br/dp/B08BTN6V7Y
