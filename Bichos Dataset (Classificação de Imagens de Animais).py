45f85fbffa1ae6749d3b74c96c5e45a8238c7cd4
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2020-08-07 15:43:29 -0300

    Update Bichos Dataset (Classificação de Imagens de Animais).py

--- Bichos Dataset (Classificação de Imagens de Animais).py
+++ Bichos Dataset (Classificação de Imagens de Animais).py
@@ -1,3 +1,7 @@
+# Livro Ciência de Dados e Aprendizado de Máquina - https://www.amazon.com.br/dp/B07X1TVLKW
+# Livro Inteligência Artificial com Python - Redes Neurais Intuitivas - https://www.amazon.com.br/dp/B087YSVVXW
+# Livro Redes Neurais Artificiais - https://www.amazon.com.br/dp/B0881ZYYCJ
+
 import numpy as np
 from keras.preprocessing import image
 from keras.models import Sequential
@@ -61,3 +65,12 @@ previsor = classificador.predict(imagem_teste)
 base_treino.class_indices
 
 resultado_final = base_treino.class_indices
+
+# Livro Python do ZERO à Programação Orientada a Objetos - https://www.amazon.com.br/dp/B07P2VJVW5
+# Livro Programação Orientada a Objetos com Python - https://www.amazon.com.br/dp/B083ZYRY9C
+# Livro Tópicos Avançados em Python - https://www.amazon.com.br/dp/B08FBKBC9H
+# Livro Ciência de Dados e Aprendizado de Máquina - https://www.amazon.com.br/dp/B07X1TVLKW
+# Livro Inteligência Artificial com Python - Redes Neurais Intuitivas - https://www.amazon.com.br/dp/B087YSVVXW
+# Livro Redes Neurais Artificiais - https://www.amazon.com.br/dp/B0881ZYYCJ
+# Livro Análise Financeira com Python - https://www.amazon.com.br/dp/B08B6ZX6BB
+# Livro Arrays com Python + Numpy - https://www.amazon.com.br/dp/B08BTN6V7Y
