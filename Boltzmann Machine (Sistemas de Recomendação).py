d22a4a575951c09f800ad9da918587bc6a2e352d
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2020-08-07 15:44:46 -0300

    Update Boltzmann Machine (Sistemas de Recomendação).py

--- Boltzmann Machine (Sistemas de Recomendação).py
+++ Boltzmann Machine (Sistemas de Recomendação).py
@@ -1,3 +1,7 @@
+# Livro Ciência de Dados e Aprendizado de Máquina - https://www.amazon.com.br/dp/B07X1TVLKW
+# Livro Inteligência Artificial com Python - Redes Neurais Intuitivas - https://www.amazon.com.br/dp/B087YSVVXW
+# Livro Redes Neurais Artificiais - https://www.amazon.com.br/dp/B0881ZYYCJ
+
 from rbm import RBM
 import numpy as np
 
@@ -33,4 +37,12 @@ for i in range(len(usuario[0])):
     if usuario[0,i] == 0 and recomendacao[0,i] == 1:
         print(filmes[i])
         
-        
+# Livro Python do ZERO à Programação Orientada a Objetos - https://www.amazon.com.br/dp/B07P2VJVW5
+# Livro Programação Orientada a Objetos com Python - https://www.amazon.com.br/dp/B083ZYRY9C
+# Livro Tópicos Avançados em Python - https://www.amazon.com.br/dp/B08FBKBC9H
+# Livro Ciência de Dados e Aprendizado de Máquina - https://www.amazon.com.br/dp/B07X1TVLKW
+# Livro Inteligência Artificial com Python - Redes Neurais Intuitivas - https://www.amazon.com.br/dp/B087YSVVXW
+# Livro Redes Neurais Artificiais - https://www.amazon.com.br/dp/B0881ZYYCJ
+# Livro Análise Financeira com Python - https://www.amazon.com.br/dp/B08B6ZX6BB
+# Livro Arrays com Python + Numpy - https://www.amazon.com.br/dp/B08BTN6V7Y
+          
