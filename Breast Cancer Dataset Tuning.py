dae4fd31cd28cadfe28884554513eb470ddada2d
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2020-08-07 15:46:35 -0300

    Update Breast Cancer Dataset Tuning.py

--- Breast Cancer Dataset Tuning.py
+++ Breast Cancer Dataset Tuning.py
@@ -40,3 +40,12 @@ tunagem = GridSearchCV(estimator = classificadorTunado,
 tunagem = tunagem.fit(entradas,saidas)
 melhores_parametros = tunagem.best_params_
 melhor_margem_precisao = tunagem.best_score_
+
+# Livro Python do ZERO à Programação Orientada a Objetos - https://www.amazon.com.br/dp/B07P2VJVW5
+# Livro Programação Orientada a Objetos com Python - https://www.amazon.com.br/dp/B083ZYRY9C
+# Livro Tópicos Avançados em Python - https://www.amazon.com.br/dp/B08FBKBC9H
+# Livro Ciência de Dados e Aprendizado de Máquina - https://www.amazon.com.br/dp/B07X1TVLKW
+# Livro Inteligência Artificial com Python - Redes Neurais Intuitivas - https://www.amazon.com.br/dp/B087YSVVXW
+# Livro Redes Neurais Artificiais - https://www.amazon.com.br/dp/B0881ZYYCJ
+# Livro Análise Financeira com Python - https://www.amazon.com.br/dp/B08B6ZX6BB
+# Livro Arrays com Python + Numpy - https://www.amazon.com.br/dp/B08BTN6V7Y
