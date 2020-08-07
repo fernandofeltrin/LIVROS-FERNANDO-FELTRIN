98d432e4a434593d975199bc4a0ae623b07843e0
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2020-08-07 15:40:28 -0300

    Update Advertising Dataset (Previsão Quantitativa).py

--- Advertising Dataset (Previsão Quantitativa).py
+++ Advertising Dataset (Previsão Quantitativa).py
@@ -1,3 +1,7 @@
+# Livro Ciência de Dados e Aprendizado de Máquina - https://www.amazon.com.br/dp/B07X1TVLKW
+# Livro Inteligência Artificial com Python - Redes Neurais Intuitivas - https://www.amazon.com.br/dp/B087YSVVXW
+# Livro Redes Neurais Artificiais - https://www.amazon.com.br/dp/B0881ZYYCJ
+
 import pandas as pd
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
@@ -32,3 +36,12 @@ mae = metrics.mean_absolute_error(steste,previsor)
 mse = metrics.mean_squared_error(steste,previsor)
 rmse = np.sqrt(metrics.mean_squared_error(steste,previsor))
 
+
+# Livro Python do ZERO à Programação Orientada a Objetos - https://www.amazon.com.br/dp/B07P2VJVW5
+# Livro Programação Orientada a Objetos com Python - https://www.amazon.com.br/dp/B083ZYRY9C
+# Livro Tópicos Avançados em Python - https://www.amazon.com.br/dp/B08FBKBC9H
+# Livro Ciência de Dados e Aprendizado de Máquina - https://www.amazon.com.br/dp/B07X1TVLKW
+# Livro Inteligência Artificial com Python - Redes Neurais Intuitivas - https://www.amazon.com.br/dp/B087YSVVXW
+# Livro Redes Neurais Artificiais - https://www.amazon.com.br/dp/B0881ZYYCJ
+# Livro Análise Financeira com Python - https://www.amazon.com.br/dp/B08B6ZX6BB
+# Livro Arrays com Python + Numpy - https://www.amazon.com.br/dp/B08BTN6V7Y
