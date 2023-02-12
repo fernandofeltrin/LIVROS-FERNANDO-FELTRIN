5ca2801e1d42ed7d2a12b5523ab440025d0e73f2
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2023-02-12 17:29:56 -0300

    Create app.py

---
+++ Blockchain-e-Criptomoedas-em-Python/app.py
@@ -0,0 +1,36 @@
+from blockchain import Blockchain
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Bloco minerado com sucesso!!!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message' : 'Blockchain validado com sucesso!!!'}
+    else:
+        response = {'message' : 'Blockchain inválido!!!'}
+    return jsonify(response), 200
+
+app.run(host = '0.0.0.0', port = 5000)
